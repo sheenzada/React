ce7b72fa8f0dfab104dadf6783e6492cfad59fa2
Inamullah <sheenzada7@gmail.com>
2026-01-31 03:55:01 -0800

    python

--- lms/src/Card.jsx
+++ lms/src/Card.jsx
@@ -1,6 +1,26 @@
 const Card = () => {
   return (
-    <div className="max-w-sm bg-white rounded-xl shadow-lg overflow-hidden">
+  
+    <div className="max-w-sm bg-white rounded-xl shadow-lg overflow-hidden mt-11 ml-12 ">
+      <img
+        src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
+        alt="Card"
+        className="w-full h-48 object-cover "
+      />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold text-gray-800">
+          Tailwind Card
+        </h2>
+
+        <p className="text-gray-600 mt-2">
+          This card is now Card ..
+        </p>
+
+        <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          Read More
+        </button>
+      </div>
       <img
         src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
         alt="Card"
@@ -20,7 +40,8 @@ const Card = () => {
           Read More
         </button>
       </div>
-    </div>
+      </div>
+    
   );
 };
 
