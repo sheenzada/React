99e379a05cc4f061012bb6df48e6ff7525493394
Inamullah <sheenzada7@gmail.com>
2026-02-01 04:23:33 -0800

    html

--- lms/src/App.jsx
+++ lms/src/App.jsx
@@ -21,7 +21,7 @@ function App () {
   return (
 
     <>
-    <Mportfolio/>
+    {/* <Mportfolio/> */}
     <Navbar/>
     <Hero/>
     <About/>
--- lms/src/Component/Navbar.jsx
+++ lms/src/Component/Navbar.jsx
@@ -2,12 +2,12 @@ const Navbar = () => {
   return (
     <nav className="fixed w-full bg-gray-900/80 backdrop-blur border-b border-gray-700 z-50">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between">
-        <h1 className="text-xl font-bold text-blue-500">MyPortfolio</h1>
+        <h1 className="text-xl font-bold text-blue-500"> <span className="text-red-500">M</span> <span className="text-green-400">y</span> Portfolio</h1>
         <div className="space-x-6 hidden md:block">
-          <a href="#home" className="hover:text-blue-400">Home</a>
+          <a href="#home" className="hover:text-white">Home</a>
           <a href="#about" className="hover:text-blue-400">About</a>
-          <a href="#projects" className="hover:text-blue-400">Projects</a>
-          <a href="#contact" className="hover:text-blue-400">Contact</a>
+          <a href="#projects" className="hover:text-red-400">Projects</a>
+          <a href="#contact" className="hover:text-yellow-400">Contact</a>
         </div>
       </div>
     </nav>
